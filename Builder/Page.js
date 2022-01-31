5347e93c3b9d0a4d12b1d22babb1534f2d550207
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-01-31 14:13:15 -0500

    Link Fix

--- Builder/Page.js
+++ Builder/Page.js
@@ -4,8 +4,8 @@ export default function(head,body){
     return `
         <html>
             <head>
-                <link rel = 'icon' type = 'image/png' href = './Resources/Favicon.png'>
-                <link rel = 'stylesheet' type = 'text/css' href = './Styles/Style.css'>
+                <link rel = 'icon' type = 'image/png' href = '/Resources/Favicon.png'>
+                <link rel = 'stylesheet' type = 'text/css' href = '/Styles/Style.css'>
                 ${ head }
             </head>
             <body>${ body }</body>
